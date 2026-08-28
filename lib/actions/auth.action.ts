561fd38465e2be54e43294e239aa0a39f624c1ff
MM120-i <mahimmarufuzzaman1234@gmail.com>
2026-08-28 14:33:56 -0400

    feat: implement authentication actions for sign-up and sign-in using Firebase

---
+++ lib/actions/auth.action.ts
@@ -0,0 +1,91 @@
+"use server";
+
+import { auth, db } from "@/firebase/admin";
+import { cookies } from "next/headers";
+import { success } from "zod";
+
+const ONE_WEEK = 60 * 60 * 24 * 7;
+
+export const signUp = async (params: SignUpParams) => {
+  const { uid, name, email } = params;
+
+  try {
+    const userRecord = await db.collection("users").doc(uid).get();
+
+    if (userRecord.exists) {
+      return {
+        success: false,
+        message: "User already exists. Please sign in instead",
+      };
+    }
+
+    await db.collection("users").doc(uid).set({
+      name,
+      email,
+    });
+
+    return {
+      success: true,
+      message: "Account created successfully, please sign in",
+    };
+  } catch (error) {
+    console.error("Error creating a user", error);
+
+    if (
+      typeof error === "object" &&
+      error !== null &&
+      "code" in error &&
+      error.code === "/auth/email-already-exists"
+    ) {
+      return {
+        success: false,
+        message: "This email is already in use.",
+      };
+    }
+
+    return {
+      success: false,
+      message: "Failed to create an account",
+    };
+  }
+};
+
+export const setSessionCookie = async (idToken: string) => {
+  const cookieStore = await cookies();
+
+  const sessionCookie = await auth.createSessionCookie(idToken, {
+    expiresIn: ONE_WEEK * 1000,
+  });
+
+  cookieStore.set("session", sessionCookie, {
+    maxAge: ONE_WEEK,
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production",
+    path: "/",
+    sameSite: "lax",
+  });
+};
+
+export const signIn = async (params: SignInParams) => {
+  const { email, idToken } = params;
+
+  try {
+    const userRecord = await auth.getUserByEmail(email);
+
+    if (!userRecord) {
+      return {
+        success: false,
+        messsage: "User does not exist. Create an account instead",
+      };
+    }
+
+    await setSessionCookie(idToken);
+  } catch (error) {
+    console.error(error);
+
+    return {
+      success: false,
+      message: "Failed to log into an account",
+    };
+  }
+};
