28ad30f9c67c857202c76d5ae98c46ccb1c292ca
MM120-i <mahimmarufuzzaman1234@gmail.com>
2026-09-01 21:26:22 -0400

    fix: update environment variable for VAPI token

--- lib/vapi.sdk.ts
+++ lib/vapi.sdk.ts
@@ -1,6 +1,6 @@
 import Vapi from "@vapi-ai/web";
 
-export const vapi = new Vapi(process.env.NEXT_PUBLIC_VAPI_WEB_TOKEN!);
+export const vapi = new Vapi(process.env.NEXT_VAPI_WEB_TOKEN!);
 
 // Boiler plait code
 // Start voice conversation
